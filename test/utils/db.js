271247f107b94e641844f966abfb745b10c1df82
Matias Lopez <matiaslopezd@gmail.com>
2021-12-02 23:26:44 -0300

    fix: key createdAt in users and orgs collection

--- test/utils/db.js
+++ test/utils/db.js
@@ -4,9 +4,9 @@ function createDb(faker) {
     const id = () => faker.fake('{{datatype.uuid}}');
 
     return {
-        users: new Array(5).fill(0).map(() => ({ id: id(), name: faker.fake('{{name.firstName}} {{name.lastName}}'), updatedAt: date() })),
+        users: new Array(5).fill(0).map(() => ({ id: id(), name: faker.fake('{{name.firstName}} {{name.lastName}}'), createdAt: date() })),
         commits: new Array(5).fill(0).map(() => ({ id: id(), createdAt: date(), sha: faker.fake('{{git.commitSha}}') })),
-        orgs: new Array(5).fill(0).map(() => ({ id: id(), name: faker.fake('{{company.companyName}}'), updatedAt: date() })),
+        orgs: new Array(5).fill(0).map(() => ({ id: id(), name: faker.fake('{{company.companyName}}'), createdAt: date() })),
     };
 
 }
